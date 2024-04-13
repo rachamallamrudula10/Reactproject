2763a5dd17a2fef2d047306a8638ef95fe2c69cb
Mrudula Rachamalla <mrudula.r@simplify3x.com>
2024-04-13 22:14:56 +0530

    React Project

---
+++ src/data.jsx
@@ -0,0 +1,31 @@
+import img1 from './Images/iphone.jpg';
+import img2 from './Images/samsung.png';
+import img3 from './Images/macbook.jpg';
+
+const data = {
+    productData:[
+        {
+            id: 1,
+            img: img1,
+            title: 'Iphone15 Pro Max',
+            desc: '',
+            price: 1200,
+        },
+        {
+            id: 2,
+            img: img2,
+            title: 'Samsung S24 Ultra',
+            desc: '',
+            price: 950,
+        },
+        {
+            id: 3,
+            img: img3,
+            title: 'Macbook Pro',
+            desc: '',
+            price: 880,
+        },
+    ],
+};
+
+export  default data;
